e784c6838aa83e4d52a88e837941bb3aa2c56e2e
Aztarok <Aztarok2994@gmail.com>
2024-02-20 22:54:21 -0500

    Added subscription page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,52 @@
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
-import Link from 'next/link';
+import Price from "@/components/subscription/price";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+import { FaGithub, FaYoutube } from "react-icons/fa";
 
 export default function Home() {
-	return (
-		<div className="flex gap-5">
-			<Link href="/dashboard">/Dashboard</Link>
-			<Link href="/profile">/Profile</Link>
-		</div>
-	);
+    return (
+        <div className="space-y-10">
+            <div>
+                <h1 className="text-xl font-bold">Text Links</h1>
+                <div className="space-x-2">
+                    <Button variant="outline" asChild>
+                        <Link href="/dashboard" className="underline">
+                            /Dashboard
+                        </Link>
+                    </Button>
+                    <Button variant="outline" asChild>
+                        <Link href="/profile" className="underline">
+                            /Profile
+                        </Link>
+                    </Button>
+                    <Button variant="outline" asChild>
+                        <Link href="/subscription" className="underline">
+                            /Subscription
+                        </Link>
+                    </Button>
+                </div>
+            </div>
+            <Price />
+            <div className=" border-t pt-10">
+                <h1 className="text-xl font-bold">
+                    Thank you cloning my boilerplate project.
+                </h1>
+                <p>If you want to support me. Follow me here</p>
+                <div className="mt-5">
+                    <div className="flex items-center gap-5">
+                        <Link href={"https://www.youtube.com/"} target="_blank">
+                            <FaYoutube className="h-8 w-8 hover:scale-105" />
+                        </Link>
+                        <Link
+                            href={"https://github.com/Aztarok"}
+                            target="_blank"
+                        >
+                            <FaGithub className="h-8 w-8 hover:scale-105" />
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
---
+++ src/components/subscription/price.tsx
@@ -0,0 +1,101 @@
+import { CheckCircle2 } from "lucide-react";
+import React from "react";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
+
+const Price = () => {
+    const prices = [
+        {
+            title: "Hobby",
+            description: "Start your side project",
+            benefits: [
+                "Improved productivity",
+                "Enhanced Performance",
+                "Cost savings",
+                "Improved communication",
+                "Enhanced Collaboration"
+            ],
+            amount: 10
+        },
+        {
+            title: "Pro",
+            description: "Start your side project",
+            benefits: [
+                "Improved productivity",
+                "Enhanced Performance",
+                "Cost savings",
+                "Improved communication",
+                "Enhanced Collaboration"
+            ],
+            amount: 20
+        },
+        {
+            title: "Enterprise",
+            description: "Start your side project",
+            benefits: [
+                "Improved productivity",
+                "Enhanced Performance",
+                "Cost savings",
+                "Improved communication",
+                "Enhanced Collaboration"
+            ],
+            amount: 100
+        }
+    ];
+    return (
+        <div>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+                {prices.map((price, index) => {
+                    const isPopular = index === 1;
+                    return (
+                        <div
+                            key={index}
+                            className={cn("border rounded-md p-5 space-y-5", {
+                                "ring-2 ring-green-500": isPopular
+                            })}
+                        >
+                            {isPopular && (
+                                <div className="inline-block">
+                                    <h1 className="text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-green-400 to-blue-400">
+                                        Most Popular
+                                    </h1>
+                                </div>
+                            )}
+                            <div className="space-y-3">
+                                <h1 className="text-2xl font-bold">
+                                    {price.title}
+                                </h1>
+                                <h1 className="text-3xl font-bold">
+                                    {price.amount}$
+                                </h1>
+                                <h1 className="text-sm text-gray-400 mt-2">
+                                    {price.description}
+                                </h1>
+                            </div>
+                            <div className="space-y-3">
+                                {price.benefits.map((benefit, index) => {
+                                    return (
+                                        <div
+                                            key={index}
+                                            className="flex items-center gap-2"
+                                        >
+                                            <CheckCircle2 />
+                                            <h1 className="text-sm text-gray-400">
+                                                {benefit}
+                                            </h1>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                            <Button variant="goldHover" className="w-full">
+                                Getting Started
+                            </Button>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
+
+export default Price;
