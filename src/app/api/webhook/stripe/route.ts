88f1a106f6173d05a7c14b4749c888e8546790f8
Aztarok <Aztarok2994@gmail.com>
2024-03-01 22:58:33 -0500

    Working on setting up stripe better

---
+++ src/app/api/webhook/stripe/route.ts
@@ -0,0 +1,33 @@
+import { headers } from "next/headers";
+import { buffer } from "node:stream/consumers";
+import Stripe from "stripe";
+
+const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET!;
+
+const stripe = new Stripe(process.env.STRIPE_SK!);
+
+export async function POST(req: any) {
+    const rawBody = await buffer(req.body);
+    try {
+        const sig = headers().get("stripe-signature");
+        let event;
+        try {
+            event = stripe.webhooks.constructEvent(
+                rawBody,
+                sig!,
+                endpointSecret
+            );
+        } catch (err: any) {
+            return Response.json({ error: `Webhook Error ${err?.message!}` });
+        }
+        switch (event.type) {
+            case "customer.subscription.deleted":
+                break;
+            default:
+                console.log(`Unhandled event type ${event.type}`);
+        }
+        return Response.json({});
+    } catch (e) {
+        return Response.json({ error: `Webhook Error` });
+    }
+}
